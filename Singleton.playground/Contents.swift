19fe6d6457e3076a19bf34b3578cb94e26590ee4
Ambrose <ambymbayi45@gmail.com>
2023-11-16 17:23:25 +0300

    create a normal class

--- Singleton.playground/Contents.swift
+++ Singleton.playground/Contents.swift
@@ -1,3 +1,18 @@
 
 // Are singletons and global instances damaging your system design and testability?
+// how singletons and global state can damage app architecture and make app harder to test. Especially in the networking layer
 
+// there are better ways and better solutions.
+
+// 1. How can I test the network layer?
+// 2. Should I be using Singletons?
+
+// what are singletons pattern? - is a way to make sure that a class has only one instance and it provides a single point of access to it. The pattern specifies that the class should be responsible itself for keeping track of its sole instance
+
+import UIKit
+
+class ApiClient {
+    
+}
+
+let client = ApiClient()
