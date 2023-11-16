2b4ef77e987c7a87e9855fea53a1296eba3cda47
Ambrose <ambymbayi45@gmail.com>
2023-11-16 18:03:00 +0300

    update the three modules to have extension of ApiClient with whatever method they need. This allows separation from concrete type

--- Singleton.playground/Contents.swift
+++ Singleton.playground/Contents.swift
@@ -9,6 +9,12 @@
 
 // what are singletons pattern? - is a way to make sure that a class has only one instance and it provides a single point of access to it. The pattern specifies that the class should be responsible itself for keeping track of its sole instance
 
+
+//NOTE: Care about Modular design - breaking this code into something manageable/responsible
+
+// the solution to this problem i.e where everytime you add a method you might to break other modules is faily simple.
+// 1. use extension - i.e we can have generic method /execute(request)/ then every module has its own apiClient Extension with whatever method they need
+
 import UIKit
 
 struct LoggedInUser {}
